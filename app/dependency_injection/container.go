dfa54a8d8c94dfdd15e0b00b10504bb802ed9e63
Louis Trezzini <louis.trezzini@ponts.org>
2018-09-23 21:43:21 +0200

    Add missing file

---
+++ app/dependency_injection/container.go
@@ -0,0 +1,33 @@
+package dependency_injection
+
+import (
+	"github.com/LouisTrezzini/foyer.enpc.org/app/services"
+	"errors"
+)
+
+type Container struct {
+	AccountRepository services.AccountRepository
+	DrinkRepository   services.DrinkRepository
+	FoyerService      services.FoyerService
+}
+
+var container *Container
+
+func GetContainer() *Container {
+	return container
+}
+
+func InitContainer() error {
+	if container != nil {
+		return errors.New("Container is already initialized")
+	}
+
+	newContainer, err := initializeContainer()
+	if err != nil {
+		return err
+	}
+
+	container = &newContainer
+
+	return nil
+}
