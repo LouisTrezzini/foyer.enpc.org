d832c01bd1359a261d31207eaab50c33c2533fdf
Louis Trezzini <louis.trezzini@ponts.org>
2019-01-25 16:18:08 +0100

    Remove default example

--- front/app/containers/LanguageProvider/tests/reducer.test.js
+++ front/app/containers/LanguageProvider/tests/reducer.test.js
@@ -1,7 +1,6 @@
 import { fromJS } from 'immutable';
 
 import languageProviderReducer from '../reducer';
-import { CHANGE_LOCALE } from '../constants';
 
 describe('languageProviderReducer', () => {
   it('returns the initial state', () => {
@@ -10,16 +9,5 @@ describe('languageProviderReducer', () => {
         locale: 'en',
       }),
     );
-  });
-
-  it('changes the locale', () => {
-    expect(
-      languageProviderReducer(undefined, {
-        type: CHANGE_LOCALE,
-        locale: 'de',
-      }).toJS(),
-    ).toEqual({
-      locale: 'de',
-    });
   });
 });
