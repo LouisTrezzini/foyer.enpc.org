ba022a72adead5515911fd5f52df745549e661ac
Louis Trezzini <louis.trezzini@ponts.org>
2019-02-03 21:23:02 +0100

    Upgrade packages

--- front/app/reducers.js
+++ front/app/reducers.js
@@ -13,13 +13,10 @@ import history from 'utils/history';
  * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers = {}) {
-  const rootReducer = combineReducers({
+  return combineReducers({
     app: appReducer,
     auth: authReducer,
+    router: connectRouter(history),
     ...injectedReducers,
   });
-
-  // Wrap the root reducer and return a new root reducer with router state
-  const mergeWithRouterState = connectRouter(history);
-  return mergeWithRouterState(rootReducer);
 }
